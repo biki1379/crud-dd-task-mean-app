acec44306b2330247017e82d3aabbc73902e6e5a
Biki Saha <116303159+biki1379@users.noreply.github.com>
2025-11-28 12:41:34 +0530

    Fix formatting in db.config.js

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,4 +1,1 @@
-module.exports = {
-  // Use Docker env variable if present, otherwise fall back for local dev
-  url: process.env.MONGO_URL || "mongodb://localhost:27017/dd_db"
-};
+module.exports = { url: process.env.MONGO_URL || "mongodb://localhost:27017/dd_db"};
